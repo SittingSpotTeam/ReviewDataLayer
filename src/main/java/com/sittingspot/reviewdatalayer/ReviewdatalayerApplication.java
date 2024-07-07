ae4687e520266e6b48b77416f4f43ec6f67ada2c
Leonardo Malcotti <leo@Leonardos-Air.lan>
2024-07-07 14:30:07 +0200

    initialized spring app

---
+++ src/main/java/com/sittingspot/reviewdatalayer/ReviewdatalayerApplication.java
@@ -0,0 +1,13 @@
+package com.sittingspot.reviewdatalayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewdatalayerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewdatalayerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sittingspot/reviewdatalayer/ReviewdatalayerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sittingspot.reviewdatalayer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewdatalayerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
