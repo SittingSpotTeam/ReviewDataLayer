4885c16d02db0ded58078ee89bcf62ee0bbc9b52
davipase <75118153+davipase@users.noreply.github.com>
2024-07-27 13:04:49 +0200

    Update ddl.sql

--- ddl.sql
+++ ddl.sql
@@ -1,8 +1,7 @@
-CREATE TABLE reviews{
-
-}
-
-CREATE TABLE reviews
+CREATE TABLE review
 (
-    corpus TEXT NOT NULL,
+    reviewId TEXT NOT NULL,
+    sittingSpotId  TEXT NOT NULL,
+    corpus  TEXT NOT NULL,
+    CONSTRAINT pk_review PRIMARY KEY (review_id)
 );
