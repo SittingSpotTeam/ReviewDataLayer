74dd1ee1af7f87e186527c587ae080144f5f2ca5
Leonardo Malcotti <leo@Leonardos-MacBook-Air.local>
2024-08-19 15:43:13 +0200

    fixed env parameters and psql driver dep

--- src/main/java/com/sittingspot/reviewdatalayer/controller/ReviewDataLayerController.java
+++ src/main/java/com/sittingspot/reviewdatalayer/controller/ReviewDataLayerController.java
@@ -12,7 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import java.util.List;
 
 @RestController
-@RequestMapping("/review-dl/api/v1")
+@RequestMapping("/api/v1")
 public class ReviewDataLayerController {
     
     @Autowired
